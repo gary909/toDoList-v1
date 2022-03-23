fb45f2fd0d9c5a666ff70b76cecbcbc6f825ab97
gary <gary.white@lodgit.com>
2022-03-23 17:07:41 +0100

    ejs

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
 // In the console you installed:
 // npm i express body-parser
+//run server
+//
+//with nodemon app.js 
 
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get("/", function(req, res){
     // res.send("Hello");
     var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
+    var dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    
 
-    if (today.getDay() === 6 || today.getDay() === 0) {
-        res.send("<h3>Yay it's the weekend</h3>")
-    } else {
-        // res.send("<h3>Boo! I have to work</h3>");
-        res.sendFile(__dirname + "/index.html");
-    }
+
+    // if (currentDay === 6 || currentDay === 0) {
+    //     // res.send("<h3>Yay it's the weekend</h3>")
+    //     day = "Weekend";
+    // } else {
+    //     // res.send("<h3>Boo! I have to work</h3>");
+    //     // res.sendFile(__dirname + "/index.html");
+    //     day = today.getDay();;
+    // }
+    res.render("list", {kindOfDay: dayOfWeek[currentDay]});
 });
 
 
