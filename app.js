8ffa3e230049070fa57447232d1dea2d4c0b981e
gary <gary.white@lodgit.com>
2022-03-24 17:42:38 +0100

    code

--- app.js
+++ app.js
@@ -9,29 +9,35 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
 app.set('view engine', 'ejs');
 
-app.get("/", function(req, res){
-    // res.send("Hello");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+
     var today = new Date();
-    var currentDay = today.getDay();
-    // var day = "";
-    var dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
 
+    var day = today.toLocaleDateString("en-US", options);
 
-    // if (currentDay === 6 || currentDay === 0) {
-    //     // res.send("<h3>Yay it's the weekend</h3>")
-    //     day = "Weekend";
-    // } else {
-    //     // res.send("<h3>Boo! I have to work</h3>");
-    //     // res.sendFile(__dirname + "/index.html");
-    //     day = today.getDay();;
-    // }
-    res.render("list", {kindOfDay: dayOfWeek[currentDay]});
+    res.render("list", { kindOfDay: day, newListItems: items });
 });
 
 
-app.listen(3000, function(){
+
+app.post("/", function (req, res) {
+    item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+});
+
+
+app.listen(3000, function () {
     console.log("Server started on port 3000");
 });
